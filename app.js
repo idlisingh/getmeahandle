903bf97213ba5f09e4a562fd42530c02a7280c19
idlisingh <idlisingh@outlook.com>
2012-08-24 22:01:07 -0400

    Using express

--- app.js
+++ app.js
@@ -1,51 +1,39 @@
-var http = require('http');
+var express = require('express');
 var fs   = require('fs');
 var idgen = require('./idgenerator');
 var url = require('url');
-var io = require('socket.io')
-
 
 var ipAddress = {}
 
-var server = http.createServer(function(request, response) {
-	var pathname = url.parse(request.url).pathname;
-	if (pathname == '/favicon.ico') {
-		response.end();
-		return;
-	}else if (pathname == '/ipaddress') {
-		var allValues = "List of IPAddress are:";
-		for (var key in ipAddress)  allValues = allValues + "\n" + key;
-		response.end(allValues);
-		return;
-	}else if (pathname == '/id') {
-		console.log('Getting Id');
-		return response.end(idgen.getId());
-	}
+var app = express();
 
-	var reqIp = request.connection.remoteAddress;
+app.configure(function() {
+});
+
+app.get('/', function(req, res) {
+	console.log('root accessed');
+	var reqIp = req.connection.remoteAddress;
 	ipAddress[reqIp] = reqIp;
-
-	response.writeHead(200, {'Content-Type': 'text/html'});
-	var rs = fs.readFileSync(__dirname + '/index.html').toString().replace('sample_id', idgen.getId());
-	response.write(rs);
-	response.end();
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	var rs = fs.readFileSync(__dirname + '/views/index.html').toString();
+	res.write(rs);
+	res.end();
 });
 
-
-var socket = io.listen(server);
-
-socket.configure('production', function () { 
-  socket.set("transports", ["xhr-polling"]); 
-  socket.set("polling duration", 0.5); 
+app.get('/id', function(req, res) {
+	console.log('Id requested');
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.write(idgen.getId());
+	res.end();
 });
 
-socket.sockets.on('connection', function (client) {
-	client.on('reqnewid', function(data) {
-		console.log(data);
-		client.emit('newid', idgen.getId());
-	});
+app.get('/ipaddress', function(req, res) {
+	console.log('getting ipaddress');
+	var allValues = "List of IPAddress are:";
+	for (var key in ipAddress)  allValues = allValues + "\n" + key;
+	res.end(allValues);
 });
 
 var port = process.env.PORT || 5000;
 console.log('Listening on port: ' + port);
-server.listen(port);
+app.listen(port);
